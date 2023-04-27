64026f27d7c1ff9497623885fd9eb7ddecaf61e5
Paul Christophe <paul.christophe6@gmail.com>
2023-04-27 16:52:16 -0400

    matching Apps

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -2,11 +2,21 @@ import { Dialog, Counter } from "solid-components";
 import { createSignal, Component } from "solid-js";
 
 const App: Component = () => {
-  const [open, setOpen] = createSignal(true);
+  const [open, setOpen] = createSignal(false);
   return (
     <div>
+      <h1>Counter</h1>
       <Counter />
-      <div style={{ border: "1px solid black", margin: "50px" }}></div>
+
+      <div
+        style={{
+          border: "1px solid black",
+          "margin-top": "50px",
+          "margin-bottom": "50px",
+        }}
+      ></div>
+
+      <h1>Dialog</h1>
       <button onClick={() => setOpen(true)}> open dialog</button>
       <Dialog onClose={() => setOpen(false)} open={open()} />
     </div>
--- packages/solid-components/src/App.tsx
+++ packages/solid-components/src/App.tsx
@@ -1,11 +1,23 @@
-import { Component, createSignal } from "solid-js";
-
+import Counter from "./components/Counter";
 import Dialog from "./components/Dialog";
+import { createSignal, Component } from "solid-js";
 
 const App: Component = () => {
   const [open, setOpen] = createSignal(false);
   return (
     <div>
+      <h2>Counter</h2>
+      <Counter />
+
+      <div
+        style={{
+          border: "1px solid black",
+          "margin-top": "50px",
+          "margin-bottom": "50px",
+        }}
+      ></div>
+
+      <h2>Dialog</h2>
       <button onClick={() => setOpen(true)}> open dialog</button>
       <Dialog onClose={() => setOpen(false)} open={open()} />
     </div>
