7758d89cd341ab95b36fd407714fdb90ea3911b0
Paul Christophe <paul.christophe6@gmail.com>
2023-04-28 09:53:14 -0400

    update config to copy files over on build

--- packages/solid-components/vite.config.ts
+++ packages/solid-components/vite.config.ts
@@ -1,27 +1,33 @@
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
-import { resolve } from "path";
+import path from "node:path";
+import dts from "vite-plugin-dts";
 import copy from "rollup-plugin-copy";
 
-export default defineConfig({
-  plugins: [
-    solidPlugin(),
-    copy({
-      targets: [{ src: "src", dest: "dist" }],
-    }),
-  ],
-  server: {
-    port: 3000,
-  },
-  build: {
-    lib: {
-      entry: resolve(__dirname, "src/index.ts"),
-      name: "components",
-      fileName: "index",
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === "production";
+
+  return {
+    plugins: [
+      solidPlugin(),
+      dts({
+        insertTypesEntry: true,
+      }),
+      isProduction &&
+        copy({
+          targets: [{ src: "src", dest: "dist" }],
+        }),
+    ].filter(Boolean),
+    build: {
+      lib: {
+        entry: path.resolve(__dirname, "src/index.ts"),
+        name: "components",
+        fileName: "index",
+      },
+      target: "esnext",
+      rollupOptions: {
+        external: ["solid-js"],
+      },
     },
-    target: "esnext",
-    rollupOptions: {
-      external: ["solid-js"],
-    },
-  },
+  };
 });
